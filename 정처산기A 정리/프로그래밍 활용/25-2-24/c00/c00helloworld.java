af82c4196a8ae3d0822d4328d62fe8f80a620188
Leticia-134340 <why8575@naver.com>
2025-02-24 14:44:43 +0900

    V0.0

---
+++ 정처산기A 정리/프로그래밍 활용/25-2-24/c00/c00helloworld.java
@@ -0,0 +1,14 @@
+package c00;
+
+public class c00helloworld //클래스 영역(객체지향 문법 적용 범위)
+{
+
+	public static void main(String[] args) //메서드 영역(절차지향 문법 적용 범위
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("HELLOWORLD");
+
+	}
+
+}
---
+++ 정처산기A 정리/프로그래밍 활용/25-2-24/c02/c04자료형.java
@@ -0,0 +1,10 @@
+package c02;
+
+public class c04자료형 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
