a5286475d2ec1db0276dd74fd59c8ff5048816cb
Leticia-134340 <why8575@naver.com>
2025-02-27 17:25:11 +0900

    add

--- 정처산기A 정리/프로그래밍 활용/25-2-27/c07/c01While.java
+++ 정처산기A 정리/프로그래밍 활용/25-2-27/c07/c01While.java
@@ -78,31 +78,50 @@ public class c01While {
 //				}
 //			}
 //			System.out.println("SUM : " +sum);
-			
-			
-			//06 N - M 까지 수중(N<M) 짝수의 합, 홀수의 합을 각 구해서 출력하세요
+		
+//		--------------------------------------------
+//		Scanner sc = new Scanner(System.in);	//공간 하나 더 생성해서 자리를 옮기는 방식
+//		int n = sc.nextInt();
+//		int m = sc.nextInt();
+//		int sum = n;
+//		if(n>=m) {
+//			int tmp = n;
+//			n = m;
+//			m = tmp;
+//		}
+//		while(n<m)
+//		{
+//			System.out.println("n : "+n);
+//			sum = sum +n;
+//			n++;
+//		} 
+//		System.out.println("sum : "+sum);
+//			
+//		-------------------------------------------------
+		//06 N - M 까지 수중(N<M) 짝수의 합, 홀수의 합을 각 구해서 출력하세요
+//		-------------------------------------------------
 		
 //		Scanner sc = new Scanner(System.in);
-//		int n = sc.nextInt(); //최소
-//		int m = sc.nextInt(); //최대
+//		int n = sc.nextInt(); //최소 직접입력
+//		int m = sc.nextInt(); //최대 직접입력
 //		int sum = n;
-//			if(n>=m) {
+//			if(n>=m) {			//n이 m보다 클 경우 서로 값을 바꾸어서 시작
 //				int tmp = n;
 //				n=m;
 //				m=tmp;
 //			}
 //			int i=n;
-//			int odd_sum = 0;	//홀수
-//			int even_sum = 0;	//짝수
+//			int odd_sum = 0;	//홀수	홀수 덧셈 값 설정 0에서 시작
+//			int even_sum = 0;	//짝수	짝수 덧셈 값 설정 0에서 시작
 //			while(i<=m)
 //			{
 //				System.out.println("i : "+i);
 //				if(i%2==0) {
 //					even_sum+=i;
-//				}
+//				}				//짝수일 경우 짝수 덧셈 값에 연산, i = n이기 때문에 최대값 이외에 굳이 위에 식에서 범위를 주지 않아도 알아서 계산이 된다
 //				else
 //					odd_sum+=i;
-//				n++;
+//				n++;			//홀수일 경우 홀수 덧셈 값에 연산, i = n이기 때문에 최대값 이외에 굳이 위에 식에서 범위를 주지 않아도 알아서 계산이 된다
 //			} 
 //			System.out.println(sum);
 //			}
@@ -115,28 +134,28 @@ public class c01While {
 //	int i=1;
 //	while(i<=n) {
 //		if(i%4==0) {
-//			System.out.println("i : "+i);
+//			System.out.println("i : "+i);		//4의 배수만 출력
 //		}
 //		else
-//			sum+=i;
+//			sum+=i;								//4의 배수가 아닌 숫자는 지나감
 //		i++;	
 	//08 구구단 N단을 출력합니다(2<=N<=9)
-		Scanner sc = new Scanner(System.in);
-		System.out.print("n 입력 : ");
-		int n = sc.nextInt();
-		
-		while(2>n || n>9)
-		{
-			System.out.println("구구단의 범위는 2 - 9 사이값을 입력하셔야합니다");
-			System.out.print("n 입력 : ");
-			n = sc.nextInt();
-		}
-		
-		int i=1;
-		while(i<10) {
-			System.out.printf("%d x %d = %d\n", n , i, n*i);
-			i++;
-		}
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("n 입력 : ");
+//		int n = sc.nextInt();
+//		
+//		while(2>n || n>9)	//2와 9 사이에 값을 입력하지 않으면 작동 x
+//		{
+//			System.out.println("구구단의 범위는 2 - 9 사이값을 입력하셔야합니다");
+//			System.out.print("n 입력 : ");
+//			n = sc.nextInt();
+//		}
+//		
+//		int i=1;
+//		while(i<10) {
+//			System.out.printf("%d x %d = %d\n", n , i, n*i);	//계산 식을 준 후 각각의 요소들의 관계 정리
+//			i++;
+//		}
 	
 	}
 
