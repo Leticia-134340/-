6b268483abd18dfa2766241e78318b13be51ca65
DESKTOP-JN7G641\seoyeon <why8575@naver.com>
2025-04-24 17:10:30 +0900

    V4.24

---
+++ 정처산기A 정리/STS/25-4-24/ex04_exception/src/main/java/com/example/app/domain/dto/PersonDto.java
@@ -0,0 +1,21 @@
+package com.example.app.domain.dto;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Component
+@Builder
+public class PersonDto {
+	private String username;
+	private int age;
+	private String addr;
+	
+
+}
---
+++ 정처산기A 정리/STS/25-4-24/ex05_dataSource/src/main/java/com/example/app/controller/SimpleController.java
@@ -0,0 +1,53 @@
+package com.example.app.controller;
+
+import java.io.FileNotFoundException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/test")
+public class SimpleController {
+	
+	  @GetMapping("/ex")
+	  public void ex1() throws FileNotFoundException {
+		  log.info("GET -"); 
+		  throw new FileNotFoundException("파일을 찾을 수 없습니다."); 
+	  }
+	
+	@RequestMapping(value = "/test1" , method = RequestMethod.GET)
+	public void test1() {
+		log.info("GET /test/test1...");
+		
+		//파라미터
+		//유효성
+		//서비스
+		//뷰이동(x) - resolve가 대신함
+	}
+	@RequestMapping(value = "/test2" , method = RequestMethod.GET)
+	public String test2() {
+		log.info("GET /test/test2...");
+		
+		return "test/abcd";
+		//파라미터
+		//유효성
+		//서비스
+		//뷰이동(x) - resulve
+	}
+	@RequestMapping(value = "/test3" , method = {RequestMethod.GET,RequestMethod.POST})
+	public void test3() {
+		log.info("GET /test/test3...");
+		
+		//파라미터
+		//유효성
+		//서비스
+		//뷰이동(x) - resolve가 대신함
+	}
+	
+
+}
